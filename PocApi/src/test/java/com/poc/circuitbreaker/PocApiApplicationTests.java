ae7d40f995e2d37b37f643a2a74533540b38e809
Rodrigo Romero <rodrigo.lima.ext@dasa.com.br>
2020-12-01 15:31:06 -0300

    start

---
+++ PocApi/src/test/java/com/poc/circuitbreaker/PocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PocCircuitBreakerHystrix/src/main/java/com/poc/circuitbreaker/PocCircuitBreakerHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.poc.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCircuitBreakerHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCircuitBreakerHystrixApplication.class, args);
+	}
+
+}
